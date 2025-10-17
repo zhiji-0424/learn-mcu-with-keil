ffdc825cd9a33a5992fb7c9c552620f275da8221
zhiji <zhiji_0424@qq.com>
2025-10-17 12:04:55 +0800

    实现暂停

--- 25081018_肖直记_task1/25081018_肖直记_task1/main.c
+++ 25081018_肖直记_task1/25081018_肖直记_task1/main.c
@@ -17,6 +17,9 @@ unsigned int timer_count = 0;
 //}
 
 
+// 流水、跑马、呼吸灯实现暂停
+unsigned char is_stop = 0;
+
 // 流水跑马时间间隔 50~450
 int wait_time_interval = 50;
 // 流水灯状态
@@ -27,26 +30,28 @@ unsigned int last_light_time = 0;
 // 流水灯(按下第一个键调整时间间隔)
 void module1()
 {
-	// 调整时间
-	if (IsKeyDown(1)) {
-		wait_time_interval += 100;
-		if (wait_time_interval > 450) {
-			wait_time_interval = 50;
+	if (!is_stop) {
+		// 调整时间
+		if (IsKeyDown(1)) {
+			wait_time_interval += 100;
+			if (wait_time_interval > 450) {
+				wait_time_interval = 50;
+			}
 		}
+		// 时间是否过了一个间隔，是否应该“流水”一格
+		if (timer_count - last_light_time < wait_time_interval)
+			return;
+		last_light_time = timer_count;
+		// 主要逻辑
+		if (water_light_status & 0x80) {
+			// 1000 0000
+			water_light_status <<= 1;
+			water_light_status += 1;
+		} else {
+			water_light_status <<= 1;
+		}
+		P2 = ~water_light_status;
 	}
-	// 时间是否过了一个间隔，是否应该“流水”一格
-	if (timer_count - last_light_time < wait_time_interval)
-		return;
-	last_light_time = timer_count;
-	// 主要逻辑
-	if (water_light_status & 0x80) {
-		// 1000 0000
-		water_light_status <<= 1;
-		water_light_status += 1;
-	} else {
-		water_light_status <<= 1;
-	}
-	P2 = ~water_light_status;
 }
 
 
@@ -56,23 +61,25 @@ char horse_light_status = 0x01;
 // 跑马灯(按第一个键调整时间间隔)
 void module2()
 {
-	// 调整时间间隔(按键事件不应阻塞)
-	if (IsKeyDown(1)) {
-		wait_time_interval += 100;
-		if (wait_time_interval > 450) {
-			wait_time_interval = 50;
+	if (!is_stop) {
+		// 调整时间间隔(按键事件不应阻塞)
+		if (IsKeyDown(1)) {
+			wait_time_interval += 100;
+			if (wait_time_interval > 450) {
+				wait_time_interval = 50;
+			}
+		}
+		// 时间是否过了一个间隔，是否应该“跑马”一次
+		if (timer_count - last_light_time > wait_time_interval) {
+			last_light_time = timer_count;
+			// 主要逻辑
+			horse_light_status <<= 1;
+			if (horse_light_status == 0) {
+				horse_light_status = 1;
+			}
+			P2 = ~horse_light_status;
 		}
 	}
-	// 时间是否过了一个间隔，是否应该“跑马”一次
-	if (timer_count - last_light_time < wait_time_interval)
-		return;
-	last_light_time = timer_count;
-	// 主要逻辑
-	horse_light_status <<= 1;
-	if (horse_light_status == 0) {
-		horse_light_status = 1;
-	}
-	P2 = ~horse_light_status;
 }
 
 
@@ -93,43 +100,45 @@ char breath_light_mode = 0;
 // 呼吸灯(按第一个键调整呼吸（亮-灭-亮）时间间隔)
 void module3()
 {
-	// 调整呼吸时间间隔
-	if (wait_time_interval == 50)
-		wait_time_interval = 2000;// 默认设置为2秒（实际时间受运行其他指令的影响，如控制语句的条件判断、跳转等，实测2.5秒）
-	if (IsKeyDown(1)) {
-		wait_time_interval += 100;
-		if (wait_time_interval > 2000) {
-			wait_time_interval = 500;
+	if (!is_stop) {
+		// 调整呼吸时间间隔
+		if (wait_time_interval == 50)
+			wait_time_interval = 2000;// 默认设置为2秒（实际时间受运行其他指令的影响，如控制语句的条件判断、跳转等，实测2.5秒）
+		if (IsKeyDown(1)) {
+			wait_time_interval += 100;
+			if (wait_time_interval > 2000) {
+				wait_time_interval = 500;
+			}
+		}
+		// 切换呼吸灯模式
+		if (IsKeyDown(3)) {
+			breath_light_mode = ~breath_light_mode;
+		}
+		// wait_time_interval 作为呼吸（亮-灭-亮）时间间隔，单位ms
+		// 使亮度变化
+		if (breath_light_mode == 0) {
+			// 线性变化
+			if (timer_count - last_animate_time > wait_time_interval/fresh_interval/2) {
+				last_animate_time = timer_count;
+				if (breathing_light>=fresh_interval)
+					breathing_velocity = -1;
+				if (breathing_light<=0)
+					breathing_velocity = 1;
+				breathing_light += breathing_velocity;
+			}
+		} else {
+			// 固定60fps更新亮度，且为sin计算预留时间，但是正弦还是太耗时
+			if (timer_count - last_animate_time > 14) {
+				last_animate_time = timer_count;
+				breathing_light = fresh_interval*0.5*(sin(3.14159*(timer_count*2.0/wait_time_interval-0.5))+1);
+			}
 		}
 	}
-	// 切换呼吸灯模式
-	if (IsKeyDown(3)) {
-		breath_light_mode = ~breath_light_mode;
-	}
-	// wait_time_interval 作为呼吸（亮-灭-亮）时间间隔，单位ms
-	// 使亮度变化
-	if (breath_light_mode == 0) {
-		// 线性变化
-		if (timer_count - last_animate_time > wait_time_interval/fresh_interval/2) {
-			last_animate_time = timer_count;
-			if (breathing_light>=fresh_interval)
-				breathing_velocity = -1;
-			if (breathing_light<=0)
-				breathing_velocity = 1;
-			breathing_light += breathing_velocity;
-		}
-	} else {
-		// 固定60fps更新亮度，且为sin计算预留时间，但是正弦还是太耗时
-		if (timer_count - last_animate_time > 14) {
-			last_animate_time = timer_count;
-			breathing_light = fresh_interval*0.5*(sin(3.14159*(timer_count*2.0/wait_time_interval-0.5))+1);
-		}
-	}
-//	P2 = ~breathing_light;
 	// 使灯刷新
 	if (timer_count - last_fresh_time > fresh_interval-breathing_light) {
 		// 保持灭一定时间，时间长度为 fresh_interval-breathing_light，再亮灯
 		P2_0 = 0;
+		// 如果灯的预定亮的时长为0，可以做实际检测使其不亮（因为后续灭灯等指令的执行需要时间，仍会亮）
 	}
 	if (timer_count - last_fresh_time > fresh_interval) {
 		last_fresh_time = timer_count;// 完成一次刷新
@@ -176,6 +185,7 @@ void main()
 			}
 			// 每次切换重置一次全局变量
 			timer_count = 0;			// 计数器清零
+			is_stop = 0;				// 流水灯、跑马灯、呼吸灯在切换后可以运行
 			wait_time_interval = 50;	// 流水、跑马、呼吸时间间隔 50~450
 			water_light_status = 0x0f;	// 流水灯状态
 			last_light_time = 0;		// 流水跑马灯上次运行的时刻
@@ -188,6 +198,10 @@ void main()
 			breath_light_mode = 0;		// 切换呼吸灯模式
 			bin_value = 0;				// 二进制要显示的值
 			P2 = 0xff;					// 灭灯
+		}
+		// 流水、跑马、呼吸灯实现暂停
+		if (IsKeyDown(4)) {
+			is_stop = ~is_stop;
 		}
 		// 运行特定模块
 		switch (module) {
